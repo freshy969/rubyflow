94105d6cdd19645f1db7dad0759e13160039a5b1
Paweł Dąbrowski <dziamber@gmail.com>
2018-06-26 14:37:26 +0200

    Update PostsController#show and use PostDecorator

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = ::Post.find(params[:id])
+    @post = ::PostDecorator.decorate(@post)
   end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -18,14 +18,21 @@ describe PostsController do
   describe 'GET #show' do
     it 'assigns post' do
       post = instance_double(Post, id: 1)
+      decorated_post = double('decorated_post')
       allow(Post).to receive(:find).with(post.id.to_s).and_return(post)
+      allow(PostDecorator).to receive(:decorate).with(
+        post
+      ).and_return(decorated_post)
 
       get :show, params: { id: post.id }
 
-      expect(assigns(:post)).to eq(post)
+      expect(assigns(:post)).to eq(decorated_post)
       expect(Post).to have_received(:find).with(post.id.to_s).once
       expect(response).to render_template(:show)
       expect(response.code).to eq('200')
+      expect(PostDecorator).to have_received(:decorate).with(
+        post
+      ).once
     end
   end
 end
