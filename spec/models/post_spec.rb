30744658363a4aa66ab89c3879b7c93431ce9f0e
Paweł Dąbrowski <dziamber@gmail.com>
2018-06-30 06:51:22 +0200

    Fix Post model specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,13 +4,12 @@ RSpec.describe Post, type: :model do
   describe 'slug generation' do
     it 'generates new slug during the post creation' do
       post = build(:post, title: 'Ruby on Rails tutorial')
-      slug_id = 'rpNFb1'
-      allow(SecureRandom).to receive(:urlsafe_base64).and_return(slug_id)
 
-      post.save
+      Timecop.travel('Sat, 05 May 2018 00:00:00 +0000 ') do
+        post.save
+      end
 
-      expect(post.slug).to eq('rpNFb1-ruby-on-rails-tutorial')
-      expect(SecureRandom).to have_received(:urlsafe_base64).once
+      expect(post.slug).to eq('5ipe40rpiwx-ruby-on-rails-tutorial')
     end
 
     it 'does not generate slug when post data is invalid' do
