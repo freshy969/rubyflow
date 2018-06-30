9522bdcdd0c24c0ad2409be7b754f363a5e6f01c
Paweł Dąbrowski <dziamber@gmail.com>
2018-07-01 00:04:14 +0200

    Update Posts::ListQuery and add support for offset

--- spec/queries/posts/list_query_spec.rb
+++ spec/queries/posts/list_query_spec.rb
@@ -8,5 +8,14 @@ describe Posts::ListQuery do
 
       expect(described_class.call).to eq([post, another_post])
     end
+
+    it 'returns 5 last posts with given offset if the offset param is passed' do
+      post_1 = Timecop.travel(5.minutes.ago) { create(:post) }
+      post_2 = Timecop.travel(6.minutes.ago) { create(:post) }
+
+      2.times { create(:post) }
+
+      expect(described_class.call(offset: 2)).to eq([post_1, post_2])
+    end
   end
 end
