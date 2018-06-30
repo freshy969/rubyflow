ca4891ceffed112be9089f3cf71f22ed4f4ba694
Paweł Dąbrowski <dziamber@gmail.com>
2018-06-30 05:10:08 +0200

    Update Posts::ListQuery and include comments and author

--- app/queries/posts/list_query.rb
+++ app/queries/posts/list_query.rb
@@ -2,7 +2,7 @@ module Posts
   class ListQuery
 
     def self.call
-      ::Post.joins(:user).order(created_at: :desc)
+      ::Post.joins(:user).includes(comments: :user).order(created_at: :desc)
     end
 
   end
