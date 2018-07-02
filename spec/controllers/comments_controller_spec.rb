957d4a9083831f927558048b9423a534722577ad
Paweł Dąbrowski <dziamber@gmail.com>
2018-07-02 14:31:55 +0200

    Update CommentsController#create and add support for the JSON request

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,56 +2,97 @@ require 'rails_helper'
 
 describe CommentsController do
   describe 'POST #create' do
-    it 'redirects to the root page if user is not signed in' do
-      post 'create', params: { comment: { body: 'body' }, post_id: 1 }
+    describe 'JSON format' do
+      context 'when user is signed in' do
+        let(:user) { create(:user, provider: 'github', uid: '123', name: 'John Doe', image_url: 'url', profile_url: 'url') }
 
-      expect(response).to redirect_to('/')
+        before do
+          @request.env["devise.mapping"] = Devise.mappings[:user]
+          sign_in(user)
+        end
+
+        it 'returns error response if comment is not valid' do
+          comment = instance_double(Comment, save: false)
+          post = instance_double(Post, id: 2, slug: 'slug')
+          body = 'comment body'
+          allow(Post).to receive(:find_by!).with(slug: post.id.to_s).and_return(post)
+          allow(Comment).to receive(:new).with(
+            user_id: user.id, post_id: post.id, body: body
+          ).and_return(comment)
+          
+          post 'create', params: { comment: { body: body }, post_id: post.id }, format: :json
+
+          expect(response.body).to eq({success: false, message: 'Please provide the comment body!'}.to_json)
+        end
+
+        it 'returns success response if post was created successfully' do
+          comment = instance_double(Comment, save: true)
+          post = instance_double(Post, id: 2, slug: "slug")
+          body = 'comment body'
+          allow(Post).to receive(:find_by!).with(slug: post.id.to_s).and_return(post)
+          allow(Comment).to receive(:new).with(
+            user_id: user.id, post_id: post.id, body: body
+          ).and_return(comment)
+
+          post 'create', params: { comment: { body: body }, post_id: post.id }, format: :json
+
+          expect(response.body).to eq({success: true, message: 'Comment added successfully!'}.to_json)
+        end
+      end
     end
 
-    context 'when user is signed in' do
-      let(:user) { create(:user, provider: 'github', uid: '123', name: 'John Doe', image_url: 'url', profile_url: 'url') }
+    describe 'HTML format' do
+      it 'redirects to the root page if user is not signed in' do
+        post 'create', params: { comment: { body: 'body' }, post_id: 1 }
 
-      before do
-        @request.env["devise.mapping"] = Devise.mappings[:user]
-        sign_in(user)
+        expect(response).to redirect_to('/')
       end
 
-      it 'shows new form if comment is not valid' do
-        comment = instance_double(Comment, save: false)
-        post = instance_double(Post, id: 2, slug: 'slug')
-        body = 'comment body'
-        allow(Post).to receive(:find_by!).with(slug: post.id.to_s).and_return(post)
-        allow(Comment).to receive(:new).with(
-          user_id: user.id, post_id: post.id, body: body
-        ).and_return(comment)
-        allow(controller).to receive(:redirect_to).with("/p/#{post.slug}",
-          gflash: { error: 'Please provide the comment body!' }
-        )
-        
-        post 'create', params: { comment: { body: body }, post_id: post.id }
+      context 'when user is signed in' do
+        let(:user) { create(:user, provider: 'github', uid: '123', name: 'John Doe', image_url: 'url', profile_url: 'url') }
 
-        expect(controller).to have_received(:redirect_to).with("/p/#{post.slug}",
-          gflash: { error: 'Please provide the comment body!' }
-        ).once
-      end
+        before do
+          @request.env["devise.mapping"] = Devise.mappings[:user]
+          sign_in(user)
+        end
 
-      it 'redirects to the post page if post was created successfully' do
-        comment = instance_double(Comment, save: true)
-        post = instance_double(Post, id: 2, slug: "slug")
-        body = 'comment body'
-        allow(Post).to receive(:find_by!).with(slug: post.id.to_s).and_return(post)
-        allow(Comment).to receive(:new).with(
-          user_id: user.id, post_id: post.id, body: body
-        ).and_return(comment)
-        allow(controller).to receive(:redirect_to).with("/p/#{post.slug}",
-          gflash: { success: 'Comment added successfully!' }
-        )
+        it 'shows new form if comment is not valid' do
+          comment = instance_double(Comment, save: false)
+          post = instance_double(Post, id: 2, slug: 'slug')
+          body = 'comment body'
+          allow(Post).to receive(:find_by!).with(slug: post.id.to_s).and_return(post)
+          allow(Comment).to receive(:new).with(
+            user_id: user.id, post_id: post.id, body: body
+          ).and_return(comment)
+          allow(controller).to receive(:redirect_to).with("/p/#{post.slug}",
+            gflash: { error: 'Please provide the comment body!' }
+          )
+          
+          post 'create', params: { comment: { body: body }, post_id: post.id }
 
-        post 'create', params: { comment: { body: body }, post_id: post.id }
+          expect(controller).to have_received(:redirect_to).with("/p/#{post.slug}",
+            gflash: { error: 'Please provide the comment body!' }
+          ).once
+        end
 
-        expect(controller).to have_received(:redirect_to).with("/p/#{post.slug}",
-          gflash: { success: 'Comment added successfully!' }
-        ).once
+        it 'redirects to the post page if post was created successfully' do
+          comment = instance_double(Comment, save: true)
+          post = instance_double(Post, id: 2, slug: "slug")
+          body = 'comment body'
+          allow(Post).to receive(:find_by!).with(slug: post.id.to_s).and_return(post)
+          allow(Comment).to receive(:new).with(
+            user_id: user.id, post_id: post.id, body: body
+          ).and_return(comment)
+          allow(controller).to receive(:redirect_to).with("/p/#{post.slug}",
+            gflash: { success: 'Comment added successfully!' }
+          )
+
+          post 'create', params: { comment: { body: body }, post_id: post.id }
+
+          expect(controller).to have_received(:redirect_to).with("/p/#{post.slug}",
+            gflash: { success: 'Comment added successfully!' }
+          ).once
+        end
       end
     end
   end
