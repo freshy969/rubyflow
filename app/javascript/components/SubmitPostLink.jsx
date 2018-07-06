a823bdc8e3581fcd576004b2bee2605592106c68
Paweł Dąbrowski <dziamber@gmail.com>
2018-07-06 13:54:38 +0200

    Use submitform hash in the URL for post form

--- app/javascript/components/SubmitPostLink.jsx
+++ app/javascript/components/SubmitPostLink.jsx
@@ -10,11 +10,19 @@ class SubmitPostLink extends React.Component {
     this.toggleNewPostForm = this.toggleNewPostForm.bind(this);
   }
 
+  componentDidMount () {
+    if(window.location.hash == "#submitform") {
+      this.toggleNewPostForm();
+    }
+  }
+
   toggleNewPostForm () {
     if(this.state.postFormHidden) {
+      window.location.hash = "submitform"
       this.setState({postFormHidden: false});
       ReactDOM.render(<NewPostForm csrf_token={this.props.csrf_token} image_url={this.props.image_url} user_signed_in={this.props.user_signed_in} />, $('#new_post_form')[0]);
     } else {
+      window.location.hash = ""
       this.setState({postFormHidden: true});
       ReactDOM.unmountComponentAtNode($('#new_post_form')[0]);
     }
